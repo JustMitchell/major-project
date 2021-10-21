18e0df480737353e4b7ad035f546f41020f31390
JustMitchell <90922288+JustMitchell@users.noreply.github.com>
2021-10-21 11:00:42 -0600

    Created a floor

--- sketch.js
+++ sketch.js
@@ -4,119 +4,159 @@ let Engine = Matter.Engine;
 let World = Matter.World;
 let Bodies = Matter.Bodies;
 
-const drawbody = Helpers.drawbody;
+// const drawbody = Helpers.drawbody;
 
 let world;
 let engine;
-let player, platform1;
+let player, platform1, ground;
 let isJumping = false;
 let playerSize = 20;
-let platform, ground;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   engine = Engine.create();
   world = engine.world;
-  platform = Bodies.rectangle(width / 4, height-50, 100, 5, {
-    isStatic: true
-  });
-  
 
-  player = new Player(width/2, height - playerSize, playerSize);
-  // platform1 = new Platform(width / 4, height - 50, 100, 5);
-
+  platform1 = new Platform(width / 4, height - 50, 100, 5);
+  ground = Bodies.rectangle(width / 2, height - 3, width, 10, {isStatic: true});
+  player = new Player(width/2, height - 50, 20);
+ 
   Engine.run(engine);
  
-  World.add(world, platform);
-
+  World.add(world, ground);
+  
   
 }
 
 function draw() {
   background(0);
-  player.display();
+  platform1.display();
+  // ground.display();
   player.movement();
-  player.keyPressed();
-  // platform1.display();
-
-  fill(255);
-  rect(platform.position.x, platform.position.y, 100, 5);
+  player.display();
+  
+  stroke(255);
+  fill("blue");
+  strokeWeight(4);
+  rectMode(CENTER);
+  rect(width / 2, height - 3, width, 10);
+  
 
 }
 
+// class Player {
+//   constructor (x, y, playerSize) {
+//     this.x = x;
+//     this.y = y;
+//     this.playerSize = playerSize;
+//     this.speedY = 1;
+//     this.speedX = 5;
+//     this.color = "red";
+//     this.gravity = 0.2;
+//     this.isJumping = false;
+//     this.acceleration = 0.3;
+//   }
+
+//   display() {
+//     fill("red");
+//     rect(this.x, this.y, this.playerSize, this.playerSize);
+//   }
+
+//   movement() {
+//     if (keyIsDown(68)) { //d
+//       this.x += this.speedX;
+//     }
+//     if (keyIsDown(65)) { // a
+//       this.x -= this.speedX;
+//     }
+//     // tp player to other size when they go off screen
+//     if (this.x > windowWidth) {
+//       this.x = 0;
+//     }
+//     if (this.x < 0) {
+//       this.x = windowWidth;
+//     }
+  
+//     //jumping
+//     if (this.y === height - this.playerSize) {
+//       this.gravity = 0;
+//       this.isJumping = false;
+//     } 
+//     else {
+//       this.gravity = 0.2;
+//     }
+  
+//     if (this.y > height - this.playerSize) {
+//       this.y = height - this.playerSize;
+//       this.speedY = 0;
+//     } 
+//     else {
+//       this.y += this.speedY;
+//       this.speedY += this.gravity;
+//     }
+//   }
+
+//   keyPressed() {
+//     if (this.isJumping === false) {
+//       if (keyCode === 32) {
+//         this.isJumping = true;
+//         console.log(isJumping);
+//         this.speedY = -5;
+//       }
+//     }
+//   }
+// }
 class Player {
-  constructor (x, y, playerSize) {
+  constructor (x, y, size) {
+    this.body = Bodies.rectangle(x, y, size, size);
+    this.size = size;
+    this.color = "red";
     this.x = x;
     this.y = y;
-    this.playerSize = playerSize;
-    this.speedY = 1;
-    this.speedX = 5;
-    this.color = "red";
-    this.gravity = 0.2;
-    this.isJumping = false;
-    this.acceleration = 0.3;
+    this.speedX = 2;
+    World.add(world, this.body);
   }
+  display () {
+    let pos = this.body.position;
 
-  display() {
-    fill("red");
-    rect(this.x, this.y, this.playerSize, this.playerSize);
+    strokeWeight(1);
+    push();
+    translate(pos.x, pos.y);
+    fill(this.color);
+    rectMode(CENTER);
+    rect(0, 0, this.size, this.size);
+    pop();
   }
-
-  movement() {
+  movement () {
     if (keyIsDown(68)) { //d
       this.x += this.speedX;
     }
     if (keyIsDown(65)) { // a
       this.x -= this.speedX;
     }
-    // tp player to other size when they go off screen
-    if (this.x > windowWidth) {
-      this.x = 0;
-    }
-    if (this.x < 0) {
-      this.x = windowWidth;
-    }
-  
-    //jumping
-    if (this.y === height - this.playerSize) {
-      this.gravity = 0;
-      this.isJumping = false;
-    } 
-    else {
-      this.gravity = 0.2;
-    }
-  
-    if (this.y > height - this.playerSize) {
-      this.y = height - this.playerSize;
-      this.speedY = 0;
-    } 
-    else {
-      this.y += this.speedY;
-      this.speedY += this.gravity;
-    }
-  }
-
-  keyPressed() {
-    if (this.isJumping === false) {
-      if (keyCode === 32) {
-        this.isJumping = true;
-        console.log(isJumping);
-        this.speedY = -5;
-      }
-    }
   }
 }
 
+
 class Platform {
   constructor (x, y, width, height) {
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-    this.color = "blue";
+    this.body = Bodies.rectangle(x, y, width, height, {
+      isStatic: true
+    });
+    this.w = width;
+    this.h = height;
+    this.color = "white";
+    World.add(world, this.body);
   }
   display() {
+    
+    let pos = this.body.position;
+  
+    push();
+    translate(pos.x, pos.y);
     fill(this.color);
-    Bodies.rectangle(this.x, this.y, this.width, this.height);
+    rectMode(CENTER);
+    rect(0, 0, this.w, this.h);
+    pop();
   }
 }
